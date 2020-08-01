963ca523d9cd149d96b5d00f4feda652678b2e1f
sam-rosenthal <ser259@cornell.edu>
2020-07-31 22:44:01 -0400

    Updates

---
+++ precache-manifest.06630cadf144d376bc15c51a92b458d0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debea3f4b9249395eae5af88eea87182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc132edb6aefccfb15f",
+    "url": "/static/css/main.10f4ece2.chunk.css"
+  },
+  {
+    "revision": "d6a2d799213342156792",
+    "url": "/static/js/10.890afcd5.chunk.js"
+  },
+  {
+    "revision": "c2b8b6250c8c9a38d900",
+    "url": "/static/js/2.3b5b3dab.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.3b5b3dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe38db32d6a06353c27",
+    "url": "/static/js/3.523c1835.chunk.js"
+  },
+  {
+    "revision": "8baa8727df1982ec4620a918911df65a",
+    "url": "/static/js/3.523c1835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2baa6a7d0a0cc03d223",
+    "url": "/static/js/4.228e7f95.chunk.js"
+  },
+  {
+    "revision": "7fe3f56f1856e3c5e5aea85f7ca4e94a",
+    "url": "/static/js/4.228e7f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d848f53dadf7d86465",
+    "url": "/static/js/5.b8378bf2.chunk.js"
+  },
+  {
+    "revision": "9fff19bc2ea5a665a999",
+    "url": "/static/js/6.c5189dd2.chunk.js"
+  },
+  {
+    "revision": "47ace9e7a4b192f35311",
+    "url": "/static/js/7.26059afe.chunk.js"
+  },
+  {
+    "revision": "5d266cfdc4f700ac46c6",
+    "url": "/static/js/8.47d58943.chunk.js"
+  },
+  {
+    "revision": "eec00743c2397d5ba350",
+    "url": "/static/js/9.e7dc31bd.chunk.js"
+  },
+  {
+    "revision": "adc132edb6aefccfb15f",
+    "url": "/static/js/main.d495b320.chunk.js"
+  },
+  {
+    "revision": "d6b611f48abc7dee97f5",
+    "url": "/static/js/runtime-main.e3f77cd1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://sam-rosenthal.github.io/precache-manifest.bab990212b7b7699236d8ce71d2587de.js"
+  "/precache-manifest.06630cadf144d376bc15c51a92b458d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://sam-rosenthal.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
